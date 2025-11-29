cd4a89b0d57fa69d37133df8104b5c4e9e8d5ff8
Coded_Bubbles <rishika.jan2007@gmail.com>
2025-11-29 17:52:51 +0530

    Update BFS_Traversal.c

--- 6_Graphs/BFS_Traversal.c
+++ 6_Graphs/BFS_Traversal.c
@@ -26,4 +26,49 @@ void BFS(int adj[MAX][MAX], int n, int start) {
         // Visit all adjacent vertices
         for (int i = 0; i < n; i++) {
             if (adj[v][i] == 1 && visited[i] == 0) {
-                queue[rear++] =
+                queue[rear++] = i;  // Enqueue
+                visited[i] = 1;
+            }
+        }
+    }
+}
+
+// Main function
+int main() {
+    int n, start;
+    int adj[MAX][MAX];
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    printf("Enter starting vertex (0 to %d): ", n - 1);
+    scanf("%d", &start);
+
+    BFS(adj, n, start);
+
+    return 0;
+}
+
+/*
+Sample Input:
+
+Enter number of vertices: 4
+Enter adjacency matrix:
+0 1 1 0
+1 0 0 1
+1 0 0 1
+0 1 1 0
+Enter starting vertex (0 to 3): 0
+
+Sample Output:
+
+BFS Traversal: 0 1 2 3
+*/
+
