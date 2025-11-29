5bf276ec9c7ff0eea0de16b18ef3cee4d273eb05
Coded_Bubbles <rishika.jan2007@gmail.com>
2025-11-29 17:55:26 +0530

    Refactor BST deletion and add inorder traversal
    Refactored the deletion function and added inorder traversal for better clarity and functionality.

--- 5_Trees/Deletion_in_BST.c
+++ 5_Trees/Deletion_in_BST.c
@@ -6,7 +6,6 @@
     Language: C
 */
 
-// Code
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -26,33 +25,100 @@ struct node* createnode(int val) {
 
 // Insert a value into BST
 struct node* insert(struct node *root, int val) {
-    if (root == NULL) {
+    if (root == NULL)
         return createnode(val);
-    }
 
-    if (val < root->data) {
+    if (val < root->data)
         root->left = insert(root->left, val);
-    } else if (val > root->data) {
+    else if (val > root->data)
         root->right = insert(root->right, val);
-    }
+
     return root;
 }
 
 // Find minimum value node in a subtree (inorder successor)
 struct node* min(struct node *node) {
-    while (node && node->left != NULL) {
+    while (node && node->left != NULL)
         node = node->left;
-    }
     return node;
 }
 
 // Delete a node from BST
 struct node* deletenode(struct node* root, int val) {
-    if (root == NULL) {
+    if (root == NULL)
         return root;
+
+    if (val < root->data)
+        root->left = deletenode(root->left, val);
+    else if (val > root->data)
+        root->right = deletenode(root->right, val);
+    else {
+        // Node with only one child or no child
+        if (root->left == NULL) {
+            struct node* temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            struct node* temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        // Node with two children: Get inorder successor
+        struct node* temp = min(root->right);
+        root->data = temp->data;
+        root->right = deletenode(root->right, temp->data);
+    }
+    return root;
+}
+
+// Inorder traversal of BST
+void inorder(struct node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+// Main function
+int main() {
+    struct node* root = NULL;
+    int n, value, del, i;
+
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    printf("Enter the values to insert:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &value);
+        root = insert(root, value);
     }
 
-    if (val < root->data) {
-        root->left = deletenode(root->left, val);
-    } else if (val > root->data) {
-        root->right = deletenode(root->right
+    printf("\nInorder traversal before deletion: ");
+    inorder(root);
+
+    printf("\nEnter the value to delete: ");
+    scanf("%d", &del);
+    root = deletenode(root, del);
+
+    printf("\nInorder traversal after deletion: ");
+    inorder(root);
+
+    return 0;
+}
+
+/*
+Sample Input:
+
+Enter the number of nodes: 5
+Enter the values to insert:
+50 30 70 20 40
+
+Enter the value to delete: 30
+
+Sample Output:
+
+Inorder traversal before deletion: 20 30 40 50 70
+Inorder traversal after deletion: 20 40 50 70
+*/
