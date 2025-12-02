203d38d060d6484b9dd3b0cdebabefb6159b0165
Coded_Bubbles <rishika.jan2007@gmail.com>
2025-12-02 19:48:55 +0530

    Update Count_Occurrences.c

--- 4_Linked_Lists/Count_Occurrences.c
+++ 4_Linked_Lists/Count_Occurrences.c
@@ -43,4 +43,51 @@ void insertEnd(struct Node** head, int value) {
 
 // Count occurrences of a given key
 int countItem(struct Node* head, int key) {
-    int cou
+    int count = 0;
+
+    while (head) {
+        if (head->data == key)
+            count++;
+
+        head = head->next;
+    }
+
+    return count;
+}
+
+// Main function
+int main() {
+    struct Node* head = NULL;
+    int n, val, key;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    // Insert all nodes
+    for (int i = 1; i <= n; i++) {
+        printf("Enter value for node %d: ", i);
+        scanf("%d", &val);
+        insertEnd(&head, val);
+    }
+
+    printf("Enter item to count: ");
+    scanf("%d", &key);
+
+    printf("%d occurs %d times in the list.\n", key, countItem(head, key));
+
+    return 0;
+}
+
+/*
+Sample Output:
+
+Enter number of nodes: 5
+Enter value for node 1: 10
+Enter value for node 2: 20
+Enter value for node 3: 10
+Enter value for node 4: 30
+Enter value for node 5: 10
+
+Enter item to count: 10
+10 occurs 3 times in the list.
+*/
