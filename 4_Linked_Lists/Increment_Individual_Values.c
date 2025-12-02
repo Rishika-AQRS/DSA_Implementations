a634c81f12eb687b407eefe9a002eff3a4d49c29
Coded_Bubbles <rishika.jan2007@gmail.com>
2025-12-02 19:51:49 +0530

    Increment each node's value by 10

--- 4_Linked_Lists/Increment_Individual_Values.c
+++ 4_Linked_Lists/Increment_Individual_Values.c
@@ -42,4 +42,48 @@ void display(struct Node* head) {
 }
 
 // Increment each node's value by 10
-void increment(struct Node* head)*
+void increment(struct Node* head) {
+    while (head) {
+        head->data += 10;
+        head = head->next;
+    }
+}
+
+// Main function
+int main() {
+    struct Node* head = NULL;
+    int n, val;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    // Insert nodes
+    for (int i = 1; i <= n; i++) {
+        printf("Enter value for node %d: ", i);
+        scanf("%d", &val);
+        insertEnd(&head, val);
+    }
+
+    printf("\nBefore increment: ");
+    display(head);
+
+    increment(head);
+
+    printf("After increment:  ");
+    display(head);
+
+    return 0;
+}
+
+/*
+Sample Output:
+
+Enter number of nodes: 3
+Enter value for node 1: 5
+Enter value for node 2: 15
+Enter value for node 3: 25
+
+Before increment: 5 -> 15 -> 25 -> NULL
+After increment:  15 -> 25 -> 35 -> NULL
+*/
+
